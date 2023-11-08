2aafd69d4bc297289f4eeefdf861f33c23b20574
Vuong Ng <vuong.qnguyen10@gmail.com>
2023-11-09 00:45:50 +1100

    update structure for each page

--- src/app/nft/page.jsx
+++ src/app/nft/page.jsx
@@ -1,39 +1,5 @@
 import NFT from 'content/NFT';
 
-import data from 'data';
-
-const Index = () => {
-  return (
-    <>
-      <header id="header" className="header-top">
-        <div className="container">
-
-          <h1>{data.name}</h1>
-          <h2 dangerouslySetInnerHTML={{ __html: data.greet }} />
-
-          <nav id="navbar" className="navbar">
-            <ul>
-              <li><div className="nav-link active" data-ref="#header">Home</div></li>
-              <li><a className="nav-link" href="/nft">NFT</a></li>
-              <li><div className="nav-link" data-ref="#about">About</div></li>
-              <li><div className="nav-link" data-ref="#resume">Resume</div></li>
-              <li><div className="nav-link" data-ref="#contact">Contact</div></li>
-            </ul>
-            <i className="bi bi-list mobile-nav-toggle"></i>
-          </nav>
-
-          <div className="social-links">
-            <a href="/assets/Vuong Quoc Nguyen - Senior Frontend Dev - Merrylands Sydney.pdf" className="resume"><i className="bi bi-file-text-fill"></i></a>
-            <a href={data.github} className="github"><i className="bi bi-github"></i></a>
-            <a href={data.linkedin} target="_blank" className="linkedin"><i className="bi bi-linkedin"></i></a>
-          </div>
-
-        </div>
-      </header>
-
-      <NFT />
-    </>
-  );
-};
+const Index = () => <NFT />;
 
 export default Index;
--- src/content/NFT/content/NFTs/index.jsx
+++ src/content/NFT/content/NFTs/index.jsx
@@ -1,26 +1,25 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import NFT from './NFT';
 
 import './style.css';
-import NFT from './NFT';
-import data from './data.json';
 
 const Index = () => {
   const [nftList, setList] = useState(data);
-  // useEffect(() => {
-  //   const run = async () => {
-  //     const [address] = await window.ethereum.request({
-  //       method: "eth_requestAccounts"
-  //     });
-  //     if (address) {
-  //       const nfts = await (await fetch(`/api/nft?address=${address}`)).json();
-  //       console.log(nfts);
-  //       setList(nfts);
-  //     }
-  //   }
-  //   run();
-  // }, []);
+  useEffect(() => {
+    const run = async () => {
+      const [address] = await window.ethereum.request({
+        method: "eth_requestAccounts"
+      });
+      if (address) {
+        const nfts = await (await fetch(`/api/nft?address=${address}`)).json();
+        console.log(nfts);
+        setList(nfts);
+      }
+    }
+    run();
+  }, []);
 
   return (
     <>
