a728d2a590056251d586c062a9000cc080932ec3
Vương Nguyễn <56904637+vuongnq10@users.noreply.github.com>
2023-11-20 23:41:44 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+      "fs/promises": false, // the solution
+    };
+
+    return config;
+  },
+}
 
 module.exports = nextConfig
