2ddaf2d47593e1525ada17517761d12c0d3061e5
Vuong Ng <vuong.qnguyen10@gmail.com>
2023-12-11 15:15:33 +1100

    update ts

--- src/api/count.ts
+++ src/api/count.ts
@@ -1,4 +1,4 @@
-export const count = async (data = {}) =>
+export const count = async (data = {}): Promise<any> =>
   await fetch('/api/count', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
