88e19301b2af2bda5abd69d767ed34e575c81a3c
Vuong Ng <vuong.qnguyen10@gmail.com>
2024-06-27 16:49:23 +1000

    update run 1

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -7,7 +7,10 @@
 // export default handler;
 
 const handler = () => {
-  console.log('Print this message every 5m.')
+  console.log('Print this message every 5m.');
+  return {
+    statusCode: 200,
+  };
 };
 
 export { handler };
