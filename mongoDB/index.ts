3620706ac9600937aaccba5596f452d0bf58e52a
Vuong Ng <vuong.qnguyen10@gmail.com>
2024-06-27 18:18:50 +1000

    update

--- mongoDB/index.ts
+++ mongoDB/index.ts
@@ -13,11 +13,12 @@ const client = new MongoClient(`${process.env.DATABASE_STR}`, {
 client.connect();
 
 export const insert = async (params: any): Promise<any> => {
-  const { data, collection } = params;
+  const { data, collection, databaseName = '' } = params;
   try {
-    const database = client.db(DATABASE_NAME);
-    const greatePotato = database.collection(collection);
-    return await greatePotato.insertOne(data);
+    // const database = client.db(databaseName || DATABASE_NAME);
+    // const greatePotato = database.collection(collection);
+    // return await greatePotato.insertOne(data);
+    return await client.db(databaseName || DATABASE_NAME).collection(collection).insertOne(data);
   } catch (error) {
     console.log(error);
   } finally {
@@ -25,14 +26,24 @@ export const insert = async (params: any): Promise<any> => {
 }
 
 export const select = async (params: any): Promise<any> => {
-  const { query = {}, collection } = params;
+  const { query = {}, collection, databaseName = '' } = params;
   try {
-    const database = client.db(DATABASE_NAME);
-    const greatePotato = database.collection(collection);
-    const data = await greatePotato.find(query).toArray();
-    return data;
+    // const database = client.db(databaseName || DATABASE_NAME);
+    // const greatePotato = database.collection(collection);
+    // const data = await greatePotato.find(query).toArray();
+    // return data;
+    return client.db(databaseName || DATABASE_NAME).collection(collection).find(query).toArray();
   } catch (error) {
     console.log(error);
   } finally {
+  }
+}
+
+export const selectOne = async (params: any): Promise<any> => {
+  const { query = {}, projection = {}, options = {}, databaseName, collection } = params;
+  try {
+    return client.db(databaseName || DATABASE_NAME).collection(collection).findOne(query, projection);
+  } catch (error) {
+    console.log(error);
   }
 }
--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,11 +1,3 @@
-// const { schedule } = require('@netlify/functions');
-
-// const handler = schedule("*/5 * * * *", () => {
-//   console.log('Print this message every 5m.')
-// });
-
-// export default handler;
-
 const handler = () => {
   console.log('Print this message every 5m.');
   return {
